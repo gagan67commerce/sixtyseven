099d3ed8f81475db24c4d57cfeeb2c6d131465ff
Sukhwinder <sukhwinder5181singh@gmail.com>
2018-08-22 10:52:39 +0530

    added Uninstall remove tables file

---
+++ Setup/Uninstall.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * FAQ Extension
+ *
+ * @package SixtySeven Commerce
+ * @author SixtySeven Commerce - All Rights Reserved
+ * @copyright 2018 SixtySeven Commerce
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+namespace SixtySeven\Faq\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class Uninstall implements UninstallInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function uninstall(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $installer = $setup;
+
+        $installer->startSetup();
+        $table = $installer->getConnection();
+        $connection->dropTable($connection->getTableName('sixtyseven_faq'));        
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_store'));
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_category'));
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_category_store'));
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_category_id'));
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_like'));
+        $connection->dropTable($connection->getTableName('sixtyseven_faq_attachment_rel'));
+        $installer->endSetup();
+    }
+}
